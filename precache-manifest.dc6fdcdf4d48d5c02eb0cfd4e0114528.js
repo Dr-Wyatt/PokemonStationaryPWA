5771f7e26540be2a2bf5699a1a2ec5311a443b70
Daniel Wyatt <daniel.ryuichi.wyatt@gmail.com>
2020-07-16 09:55:59 -0500

    Updates

---
+++ precache-manifest.dc6fdcdf4d48d5c02eb0cfd4e0114528.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bba48ff4e70b43a640cc867e7f390875",
+    "url": "/PokemonStationaryPWA/index.html"
+  },
+  {
+    "revision": "d777f9d9e0f9b8adc788",
+    "url": "/PokemonStationaryPWA/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "953e7d9f098cb12495fa",
+    "url": "/PokemonStationaryPWA/static/js/2.7745e02d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PokemonStationaryPWA/static/js/2.7745e02d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d777f9d9e0f9b8adc788",
+    "url": "/PokemonStationaryPWA/static/js/main.58ad0ead.chunk.js"
+  },
+  {
+    "revision": "c48a24774c89b1fbb92e",
+    "url": "/PokemonStationaryPWA/static/js/runtime-main.07396719.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e94df698460a57ac442ee7df7d7348f3.js"
+  "/PokemonStationaryPWA/precache-manifest.dc6fdcdf4d48d5c02eb0cfd4e0114528.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PokemonStationaryPWA/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
